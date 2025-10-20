3aeb2f706714fd62c00078ac687f8bcceb77f7fc
eroys <eroys@aiminspections.com>
2025-10-20 18:15:43 -0400

    restyle reset button

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,9 +7,11 @@ import {
   SelectPlayingAs,
   SelectStatus,
 } from '../model/gameReducer';
+import { PiRecycleLight } from 'react-icons/pi';
 
 import { useAppDispatch, useAppSelector } from '../store/storeHooks';
 import { Cell } from './Square';
+import StyledIconText from '../lib/styledIconText';
 // import { CloseModal } from './modalClose';
 
 export const Board = () => {
@@ -31,18 +33,22 @@ export const Board = () => {
     nextMoving();
   }, [nextMove, myPlayer, dispatch, status]);
 
+  const doReset = () => {
+    dispatch(reset())
+  }
+
   return (
     <>
-      <button
-        style={{ backgroundColor: 'white' }}
-        onClick={() => dispatch(reset())}>
-        Reset
-      </button>
-      <div
-        className={`drop-shadow-custom-m-gray flex flex-wrap w-1/3`}>
-
+      <StyledIconText
+        icon={PiRecycleLight}
+        text='Reset'
+        iconClass='text-black'
+        txtClass='text-black pr-5 drop-shadow-custom-m-gray cursor-pointer'
+        onClick={() => doReset()}
+      />
+      <div className={`drop-shadow-custom-m-gray flex flex-wrap w-1/3`}>
         {squares.map((s) => (
-          <Cell key={s.id} id={s.id}/>
+          <Cell key={s.id} id={s.id} />
         ))}
       </div>
       {/* <CloseModal isOpen={isOpen} onClose={closeModal} children={<></>} /> */}
